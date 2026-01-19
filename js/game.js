e1f25fd4549a47440c0ce617f51ffdcb1e1ecf16
Jorandint <116171114+Jorandint@users.noreply.github.com>
2026-01-19 09:30:14 +0100

    Change game height from 900 to 800

--- js/game.js
+++ js/game.js
@@ -124,7 +124,7 @@ var config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1200,
-        height: 900,
+        height: 800,
         expandParent: true,
         resizeInterval: 50,
       },
